67155e4b27e3f06e6e356262800822b3e26bdf04
Flomm <florian.baranya@gmail.com>
2026-03-01 11:48:35 +0100

    add api route

--- src/app.ts
+++ src/app.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import swaggerUi from 'swagger-ui-express';
 import swaggerJSDoc from 'swagger-jsdoc';
 import config from './config';
+import apiRouter from './routes/api.routes';
 
 const app = express();
 const swaggerDocs = swaggerJSDoc(config.swaggerOptions);
 
+app.use('/api', apiRouter);
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 export default app;
